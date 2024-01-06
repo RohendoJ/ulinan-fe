a13378a121ee0313e5fd159879113d97fb964a58
RohendoJ <rohendoj3134.github@gmail.com>
2024-01-06 21:26:44 +0700

    improve: slideshow

--- src/pages/dashboard/user/slideshow.jsx
+++ src/pages/dashboard/user/slideshow.jsx
@@ -1,14 +1,7 @@
 import { Fade } from "react-slideshow-image";
 
 export const Slideshow = () => {
-  const images = [
-    "/panorama-1.jpg",
-    "/panorama-2.jpg",
-    "/panorama-3.jpg",
-    "/panorama-4.jpg",
-    "/panorama-5.jpeg",
-    "https://images.unsplash.com/photo-1615024420475-cc7f2fe24f6e?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  ];
+  const images = ["/tour/tour1.png", "/tour/tour2.png", "/tour/tour3.png"];
 
   return (
     <section className="mt-7 xl:mt-28 w-full h-[200px] md:h-[300px] flex justify-center items-center">
@@ -17,7 +10,8 @@ export const Slideshow = () => {
           arrows={false}
           autoplay={true}
           transitionDuration={1000}
-          duration={2000}>
+          duration={2000}
+        >
           {images.map((image, index) => (
             <img
               key={index}
