8b91b34a8e41b1f972e4f6ea04c3c8322817307b
EL5225 <luthfiramadhan155@gmail.com>
2024-01-07 20:04:28 +0700

    bugfix: images

--- src/pages/dashboard/user/product-detail.jsx
+++ src/pages/dashboard/user/product-detail.jsx
@@ -24,7 +24,8 @@ export const ProductDetail = () => {
   }, [data?.data]);
 
   const [image, setImage] = useState(
-    "https://images.unsplash.com/photo-1503503330041-4cd943d2b61f?q=80&w=1531&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    product?.image?.[0]?.image_url ||
+      "https://images.unsplash.com/photo-1503503330041-4cd943d2b61f?q=80&w=1531&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   );
 
   const handleThumbnailClick = (url) => {
